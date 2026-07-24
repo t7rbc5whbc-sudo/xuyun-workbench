a499a3f49eedeee2ac27ec7e2153b725316e4ae6
t7rbc5whbc-sudo <t7rbc5whbc-sudo@users.noreply.github.com>
2026-07-24 18:30:54 +0800

    徐云工作台 - 每日计划与专注计时App

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'xuyun-workbench-v1';
+const ASSETS = [
+  '/index.html',
+  '/manifest.json',
+  '/icon.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
+});
